641f0f3d2b209f3b3f55c6a7188e68dda56ec5cc
EC2 Default User <ec2-user@ip-172-31-39-5.ap-northeast-1.compute.internal>
2023-03-07 03:40:12 +0000

    [finish]コメント投稿機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "homes/about" => "homes#about", as:"about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :post_images, only: [:new, :index, :show, :create, :destroy]do
+    resources :post_comments, only: [:create]
+  end
   resources :users, only: [:show, :edit,:update]
 
 end
